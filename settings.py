3c9c824079fa98a8b0c8ee7fdcd056b3c653db72
mercurate <mercurate@163.com>
2011-10-31 23:29:57 +0800

    hi

--- settings.py
+++ settings.py
@@ -1,4 +1,11 @@
-# Django settings for mysite project.
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Django settings for keyctrl project.
+
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -100,12 +107,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'keyctrl.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(HERE, 'templates')
 )
 
 INSTALLED_APPS = (
