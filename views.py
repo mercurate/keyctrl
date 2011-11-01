ffd73aff0ad0d6eb20de91c1caeeda08f9790a22
mercurate <mercurate@163.com>
2011-11-01 22:19:33 +0800

    add sendkeys files

--- views.py
+++ views.py
@@ -5,10 +5,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import os, sys, tempfile
+from SendKeys import SendKeys
+
+KEY_MAP = {'left': '{LEFT}',
+           'right': '{ROGHT}',
+           'up': '{UP}',
+           'down': '{DOWN}',
+           'space': '{SPACE}',
+           'enter': '{ENTER}',
+           'shutdown': "{LWIN}rshutdown{SPACE}-s{SPACE}-t{SPACE}1{ENTER}"
+          }
+
 def home(request):
     return render(request, 'main.html', {})
     
 def cmd(request):
     c = request.GET['c']
     print c
+    SendKeys(KEY_MAP[c])
     return HttpResponse('')
