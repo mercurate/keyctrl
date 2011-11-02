8b4b83081e5a33f7531af259df6a29465b80bd67
mercurate <mercurate@163.com>
2011-11-02 23:09:37 +0800

    new keys

--- views.py
+++ views.py
@@ -20,18 +20,36 @@ def home(request):
     
 def key(request):
     try:
-        k = request.GET['k']
-        SendKeys(k, pause = 0.5)
-        print k
+        key = request.GET['key']
+        ctrl = request.GET['ctrl']
+        alt = request.GET['alt']
+        shift = request.GET['shift']
+        
+        key = '{%s}' % key
+        
+        if ctrl == 'true':
+            print 'ctrl'
+            key = '^' + key
+
+        if alt == 'true':
+            print 'alt'        
+            key = '%' + key
+            
+        if shift == 'true':
+            print 'shift'        
+            key = '+' + key
+            
+        SendKeys(key, pause = 0.5)
+        print key
         return HttpResponse('')
     except Exception, e:
         print e
         
 def cmd(request):
     try:
-        k = request.GET['k']
-        SendKeys(CMD_MAP[k], pause = 0.5)
-        print k
+        c = request.GET['cmd']
+        SendKeys(CMD_MAP[c], pause = 0.01)
+        print c
         return HttpResponse('')
     except Exception, e:
         print e        
