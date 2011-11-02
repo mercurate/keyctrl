f62e0f5e97b79c19695f8723dbed8850c20306ad
mercurate <mercurate@163.com>
2011-11-02 13:51:06 +0800

    new keys

--- views.py
+++ views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 import os, sys, tempfile
-from SendKeys import SendKeys
+#~ from SendKeys import SendKeys
 
 KEY_MAP = {'left': '{LEFT}',
            'right': '{RIGHT}',
@@ -18,10 +18,11 @@ KEY_MAP = {'left': '{LEFT}',
           }
 
 def home(request):
-    return render(request, 'main.html', {})
+    Fn = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+    return render(request, 'main2.html', {'Fn': Fn})
     
 def cmd(request):
     c = request.GET['c']
     print c
-    SendKeys(KEY_MAP[c])
+    #~ SendKeys(KEY_MAP[c])
     return HttpResponse('')
