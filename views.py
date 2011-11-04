5d39825a121cc249f032a0440da0bcba6b595c28
mercurate <mercurate@163.com>
2011-11-04 23:49:16 +0800

    better fn and alphabet

--- views.py
+++ views.py
@@ -5,52 +5,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import os, sys, tempfile
+import os, sys, string, tempfile
 from SendKeys import SendKeys
 
 CMD_MAP = {
-           'standby': "{LWIN}us",
+           'standby':  "{LWIN}us",
            'shutdown': "{LWIN}uu",
-           'restart': "{LWIN}ur",
+           'restart':  "{LWIN}ur",
           }
 
 def home(request):
-    # Fn = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
-    Fn = [[1,2,3], [4,5,6], [7,8,9], [10,11,12]]
-    return render(request, 'keys.html', {'Fn': Fn})
+    A = string.ascii_uppercase
+    N = range(1,13)
+    return render(request, 'keys.html', {'N': N, 'A': A})
     
 def key(request):
-    try:
-        key = request.GET['key']
-        ctrl = request.GET['ctrl']
-        alt = request.GET['alt']
-        shift = request.GET['shift']
-        
-        key = '{%s}' % key
-        
-        if ctrl == 'true':
-            print 'ctrl'
-            key = '^' + key
+    key = request.GET['key']
+    ctrl = request.GET['ctrl']
+    alt = request.GET['alt']
+    shift = request.GET['shift']
+    
+    key = '{%s}' % key
+    
+    if ctrl == 'true':
+        print 'ctrl'
+        key = '^' + key
 
-        if alt == 'true':
-            print 'alt'        
-            key = '%' + key
-            
-        if shift == 'true':
-            print 'shift'        
-            key = '+' + key
-            
-        SendKeys(key, pause = 0.5)
-        print key
-        return HttpResponse('')
-    except Exception, e:
-        print e
+    if alt == 'true':
+        print 'alt'        
+        key = '%' + key
+        
+    if shift == 'true':
+        print 'shift'        
+        key = '+' + key
+        
+    SendKeys(key, pause = 0.5)
+    print key
+    return HttpResponse('')
         
 def cmd(request):
-    try:
-        c = request.GET['cmd']
-        SendKeys(CMD_MAP[c], pause = 0.01)
-        print c
-        return HttpResponse('')
-    except Exception, e:
-        print e        
+    c = request.GET['cmd']
+    SendKeys(CMD_MAP[c], pause = 0.01)
+    print c
+    return HttpResponse('')     
